07b67fda33138993deb4a4aa15dfdfebd6302d56
pratima-sapkota <sapkotapratima0@gmail.com>
2021-06-21 14:15:00 -0400

    Modernize UI with grid layout and ttk styling
    - Refactor main_window.py into MainWindow class
    - Replace place() with grid() layout for responsiveness
    - Use ttk themed widgets (ttk.Button, ttk.Combobox, ttk.Entry)
    - Add 'clam' theme for modern look
    - Configure consistent fonts and padding via ttk.Style
    - Reorder form fields: Source -> Destination -> Speed (logical flow)
    - Add speed hint label showing valid range
    - Extract coordinate lookup into _get_coordinates method

--- ambulance_gis/ui/main_window.py
+++ ambulance_gis/ui/main_window.py
@@ -2,11 +2,13 @@
 Ambulance GIS System - Main Window
 
 The main application window with input controls for the simulation.
+Uses grid layout and ttk themed widgets for a modern look.
 """
 
 import tkinter as tk
 from tkinter import ttk, messagebox
 import csv
+from typing import Optional, Tuple
 
 from config import paths, ui_config
 from ambulance_gis.utils.validators import (
@@ -17,45 +19,195 @@ from ambulance_gis.utils.validators import (
 )
 
 
-def run() -> None:
-    """Run the main application window."""
-    window = tk.Tk()
-    window.configure(background=ui_config.BACKGROUND_COLOR)
+class MainWindow:
+    """
+    Main application window for the Ambulance GIS System.
 
-    def start() -> None:
+    Uses ttk themed widgets and grid layout for a modern,
+    responsive interface.
+
+    Attributes:
+        window: The root Tk window.
+        source: Combobox for source location selection.
+        destination: Combobox for destination location selection.
+        speed: Entry for speed input.
+    """
+
+    def __init__(self) -> None:
+        """Initialize the main window."""
+        self.window = tk.Tk()
+        self.window.title(ui_config.WINDOW_TITLE)
+        self.window.geometry(ui_config.WINDOW_SIZE)
+        self.window.configure(background=ui_config.BACKGROUND_COLOR)
+
+        # Configure ttk style
+        self._setup_style()
+
+        # Configure grid weights for responsiveness
+        self.window.columnconfigure(0, weight=1)
+        self.window.columnconfigure(1, weight=2)
+
+        # Build the UI
+        self._create_widgets()
+
+    def _setup_style(self) -> None:
+        """Configure ttk styling for a modern look."""
+        style = ttk.Style()
+        style.theme_use('clam')
+
+        # Configure common styles
+        style.configure(
+            'TLabel',
+            background=ui_config.BACKGROUND_COLOR,
+            font=('Helvetica', 10)
+        )
+        style.configure(
+            'Title.TLabel',
+            background=ui_config.BACKGROUND_COLOR,
+            font=('Helvetica', 20, 'bold')
+        )
+        style.configure(
+            'Info.TLabel',
+            background=ui_config.BACKGROUND_COLOR,
+            font=('Helvetica', 9)
+        )
+        style.configure(
+            'TButton',
+            font=('Helvetica', 10),
+            padding=10
+        )
+        style.configure(
+            'TCombobox',
+            padding=5
+        )
+        style.configure(
+            'TEntry',
+            padding=5
+        )
+
+    def _create_widgets(self) -> None:
+        """Create and layout all widgets."""
+        # Title
+        title_label = ttk.Label(
+            self.window,
+            text=ui_config.WINDOW_TITLE,
+            style='Title.TLabel'
+        )
+        title_label.grid(row=0, column=0, columnspan=2, pady=(20, 10))
+
+        # Info section
+        info_frame = ttk.Frame(self.window)
+        info_frame.grid(row=1, column=0, columnspan=2, pady=(0, 20), padx=20, sticky='w')
+
+        info_texts = [
+            ">> Enter your location, speed and destination",
+            ">> See realtime traffic at different places",
+            ">> Get the best path suggested in the map",
+        ]
+
+        for i, text in enumerate(info_texts):
+            info_label = ttk.Label(info_frame, text=text, style='Info.TLabel')
+            info_label.grid(row=i, column=0, sticky='w', pady=2)
+
+        # Form section - using a frame for better organization
+        form_frame = ttk.Frame(self.window)
+        form_frame.grid(row=2, column=0, columnspan=2, pady=10, padx=40)
+        form_frame.columnconfigure(1, weight=1)
+
+        # Source (Where are you?)
+        source_label = ttk.Label(form_frame, text="Source:")
+        source_label.grid(row=0, column=0, sticky='e', padx=(0, 10), pady=10)
+
+        self.source = ttk.Combobox(
+            form_frame,
+            values=ui_config.LOCATIONS,
+            state='readonly',
+            width=20
+        )
+        self.source.grid(row=0, column=1, sticky='w', pady=10)
+
+        # Destination
+        dest_label = ttk.Label(form_frame, text="Destination:")
+        dest_label.grid(row=1, column=0, sticky='e', padx=(0, 10), pady=10)
+
+        self.destination = ttk.Combobox(
+            form_frame,
+            values=ui_config.LOCATIONS,
+            state='readonly',
+            width=20
+        )
+        self.destination.grid(row=1, column=1, sticky='w', pady=10)
+
+        # Speed
+        speed_label = ttk.Label(form_frame, text="Speed:")
+        speed_label.grid(row=2, column=0, sticky='e', padx=(0, 10), pady=10)
+
+        self.speed = ttk.Entry(form_frame, width=23)
+        self.speed.grid(row=2, column=1, sticky='w', pady=10)
+
+        # Speed hint
+        speed_hint = ttk.Label(
+            form_frame,
+            text="(1-200 units)",
+            style='Info.TLabel'
+        )
+        speed_hint.grid(row=2, column=2, sticky='w', padx=(5, 0))
+
+        # Start button
+        button_frame = ttk.Frame(self.window)
+        button_frame.grid(row=3, column=0, columnspan=2, pady=30)
+
+        start_button = ttk.Button(
+            button_frame,
+            text="Start Driving",
+            command=self._on_start_click
+        )
+        start_button.grid(row=0, column=0)
+
+    def _get_coordinates(self, location_name: str) -> Optional[Tuple[int, int]]:
         """
-        Start the ambulance simulation.
+        Get coordinates for a location name from the CSV file.
 
-        Reads source and destination locations from the UI, validates input,
-        and launches the simulation with the specified parameters.
+        Args:
+            location_name: Name of the location to look up.
+
+        Returns:
+            Tuple of (x, y) coordinates or None if not found.
         """
         try:
-            # Validate inputs
-            validate_location(source.get(), "Source")
-            validate_location(destination.get(), "Destination")
-            validate_route(source.get(), destination.get())
-            s = validate_speed(speed.get())
-
-            # Parse coordinates from CSV
-            sour = None
-            dest = None
-
             with open(paths.POINTS_FILE, "r") as f:
-                roads = csv.reader(f)
-                next(roads)
-                for row in roads:
-                    if destination.get() == row[6]:
-                        dest = (int(row[0]), int(row[1]))
-                    if source.get() == row[6]:
-                        sour = (int(row[0]), int(row[1]))
+                reader = csv.reader(f)
+                next(reader)  # Skip header
+                for row in reader:
+                    if row[6] == location_name:
+                        return (int(row[0]), int(row[1]))
+        except (FileNotFoundError, IndexError, ValueError):
+            pass
+        return None
 
-            if sour is None or dest is None:
-                messagebox.showerror("Error", "Could not find coordinates for selected locations")
+    def _on_start_click(self) -> None:
+        """Handle the Start Driving button click."""
+        try:
+            # Validate inputs
+            validate_location(self.source.get(), "Source")
+            validate_location(self.destination.get(), "Destination")
+            validate_route(self.source.get(), self.destination.get())
+            speed_value = validate_speed(self.speed.get())
+
+            # Get coordinates
+            source_coords = self._get_coordinates(self.source.get())
+            dest_coords = self._get_coordinates(self.destination.get())
+
+            if source_coords is None or dest_coords is None:
+                messagebox.showerror(
+                    "Error",
+                    "Could not find coordinates for selected locations"
+                )
                 return
 
             # Start the simulation
             from ambulance_gis.core.simulation import main_function
-            main_function(sour, dest, s)
+            main_function(source_coords, dest_coords, speed_value)
 
         except ValidationError as e:
             messagebox.showerror("Validation Error", e.message)
@@ -64,46 +216,15 @@ def run() -> None:
         except Exception as e:
             messagebox.showerror("Error", f"An unexpected error occurred: {str(e)}")
 
-    var = tk.StringVar()
-    var.set("Pulchowk")
+    def run(self) -> None:
+        """Start the main event loop."""
+        self.window.mainloop()
 
-    lbl = tk.Label(window, text=ui_config.WINDOW_TITLE, fg='black', font=("Helvetica", 20),
-                   bg=ui_config.BACKGROUND_COLOR)
-    lbl.place(x=70, y=20)
 
-    lbl1 = tk.Label(window, text=">> Enter your location speed and destination", fg='black', font=("Helvetica", 8),
-                 bg=ui_config.BACKGROUND_COLOR)
-    lbl1.place(x=80, y=100)
-    lbl2 = tk.Label(window, text=">> See Realtime Traffic at different places", fg='black', font=("Helvetica", 8),
-                 bg=ui_config.BACKGROUND_COLOR)
-    lbl2.place(x=80, y=120)
-    lbl3 = tk.Label(window, text=">> Get the best path suggested in the map", fg='black', font=("Helvetica", 8),
-                 bg=ui_config.BACKGROUND_COLOR)
-    lbl3.place(x=80, y=140)
-
-    lbl4 = tk.Label(window, text="Destination", fg='black', font=("Helvetica", 10), bg=ui_config.BACKGROUND_COLOR)
-    lbl4.place(x=160, y=180)
-
-    destination = ttk.Combobox(window, values=ui_config.LOCATIONS)
-    destination.place(x=130, y=200)
-
-    lbl5 = tk.Label(window, text="Speed", fg='black', font=("Helvetica", 10), bg=ui_config.BACKGROUND_COLOR)
-    lbl5.place(x=160, y=230)
-    speed = tk.Entry()
-    speed.place(x=140, y=250)
-
-    lbl6 = tk.Label(window, text="Where are you?", fg='black', font=("Helvetica", 10), bg=ui_config.BACKGROUND_COLOR)
-    lbl6.place(x=150, y=280)
-
-    source = ttk.Combobox(window, values=ui_config.LOCATIONS)
-    source.place(x=130, y=300)
-
-    btn = tk.Button(window, text="Start Driving", fg='white', bg='black', command=start)
-    btn.place(x=160, y=400)
-
-    window.title(ui_config.WINDOW_TITLE)
-    window.geometry(ui_config.WINDOW_SIZE)
-    window.mainloop()
+def run() -> None:
+    """Run the main application window."""
+    app = MainWindow()
+    app.run()
 
 
 if __name__ == "__main__":
